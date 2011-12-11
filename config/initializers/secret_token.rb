c6f1fcb0ec36e7becad2ba7f69f103a2f2b0495e
Marc Remolt <marc.remolt@googlemail.com>
2011-12-11 21:11:26 +0100

    Initial commit
    * new rails app
    * base gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuestbookDemo::Application.config.secret_token = 'be2a5c0e51e00d55c2544c7e540bfb61d8be4703863101bcfb792094e605b1d9df32f9acc60f8c7928ccafccae778583d8f7bfe59b18af8344555b86c1734a89'
